d9c41fae620abda43649b8217abd735d04a897d4
tsy <tangsiyu999@163.com>
2023-01-18 14:16:27 +0800

    1.18 显示我收到的赞/关注、取消关注

--- src/main/java/com/nowcoder/community/service/LikeService.java
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -2,7 +2,10 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,17 +14,44 @@ public class LikeService {
     @Autowired
     private RedisTemplate redisTemplate; //数据放入redis
 
-    //点赞
-    public void like(int userId, int entityType, int entityId){
-        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId); //redisKey
-        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);//判断是否已经点过赞
-        if (isMember){
-            //点过赞
-            redisTemplate.opsForSet().remove(entityLikeKey, userId);
-        } else {
-            //没点过赞
-            redisTemplate.opsForSet().add(entityLikeKey, userId);
-        }
+
+    //点赞(两次更新Redis)(一项业务有两次存储==>开启事务)
+    public void like(int userId, int entityType, int entityId, int entityUserId){
+//        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId); //redisKey
+//        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);//判断是否已经点过赞
+//        if (isMember){
+//            //点过赞
+//            redisTemplate.opsForSet().remove(entityLikeKey, userId);
+//        } else {
+//            //没点过赞
+//            redisTemplate.opsForSet().add(entityLikeKey, userId);
+//        }
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+                String userLikeKey = RedisKeyUtil.getUserLikeKey(entityUserId);
+
+                //查询要在事务之外，不然Redis不会立即执行
+                boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);//判断是否已经点过赞
+
+                //开启事务
+                operations.multi();
+
+                if (isMember){
+                    //取消点赞
+                    operations.opsForSet().remove(entityLikeKey, userId);
+                    operations.opsForValue().decrement(userLikeKey);
+                }else {
+                    //点赞
+                    operations.opsForSet().add(entityLikeKey, userId);
+                    operations.opsForValue().increment(userLikeKey);
+                }
+
+                return operations.exec();
+            }
+        });
+
     }
 
     // 查询某实体点赞的数量
@@ -34,5 +64,12 @@ public class LikeService {
     public int findEntityLikeStatus(int userId, int entityType, int entityId){
         String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId); //redisKey
         return redisTemplate.opsForSet().isMember(entityLikeKey, userId) ? 1 : 0;
+    }
+
+    // 查询某用户获得的赞的数量
+    public int findUserLikeCount(int userId){
+        String userLikeKey = RedisKeyUtil.getUserLikeKey(userId);
+        Integer count = (Integer) redisTemplate.opsForValue().get(userLikeKey);
+        return count == null? 0 : count;
     }
 }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -4,6 +4,9 @@ public class RedisKeyUtil {
 
     private static final String SPLIT = ":"; //声明命名中一个分割的常量
     private static final String PREFIX_ENTITY_LIKE = "like:entity"; //实体赞的前缀
+    private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee"; //统计关注目标
+    private static final String PREFIX_FOLLOWER = "follower"; //统计粉丝
 
     // 生成某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
@@ -11,4 +14,21 @@ public class RedisKeyUtil {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
     }
 
+    // 某个用户的赞
+    // like:user:userId -> int
+    public static String getUserLikeKey(int userId){
+        return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    //某个用户关注的实体
+    // followee: userId: entityType -> zset(entityId, nowTime)
+    public static String getFolloweeKey(int userId, int entityType){
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    //某个实体拥有的粉丝
+    //follower: entityType: entityId -> zset(userId, nowTime)
+    public static String getFollowerKey(int entityType, int entityId){
+        return PREFIX_FOLLOWER +SPLIT + entityType + SPLIT + entityId;
+    }
 }
