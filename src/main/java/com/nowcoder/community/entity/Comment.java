3294ef64b3ef96962f91e6c3a1bb0638806a7509
Lluvia-Tang <tangsiyu999@163.com>
2023-06-27 09:50:29 +0800

    使用Lombok简化实体类

--- src/main/java/com/nowcoder/community/entity/Comment.java
+++ src/main/java/com/nowcoder/community/entity/Comment.java
@@ -1,7 +1,13 @@
 package com.nowcoder.community.entity;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
     private int id;
     private int userId;
